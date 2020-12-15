35c09e83dba292e169280ab0eb981704d1cd7ff5
Petr <monastyrevpetr@gmail.com>
2020-12-15 13:07:13 +0300

    hw7

--- hw7/src/Main.java
+++ hw7/src/Main.java
@@ -2,11 +2,23 @@
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph(5);
-        graph.addEdge(1,2);
-        graph.addEdge(0,4);
-        graph.addEdge(1,4);
-        graph.addEdge(1,0);
+        Graph graph = new Graph(10);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 6);
+        graph.addEdge(6, 7);
+        graph.addEdge(7, 8);
+        graph.addEdge(8, 9);
+        graph.addEdge(0, 4);
+        graph.addEdge(4, 9);
+        graph.addEdge(5, 1);
+        graph.addEdge(3, 5);
+        graph.addEdge(4, 7);
+        graph.addEdge(1, 8);
+        graph.addEdge(5, 0);
 
 //        System.out.println(graph.getAdjList(1));
 
@@ -15,9 +27,10 @@ public class Main {
 //        System.out.println(dfp.pathTo(0));
 
 
-        BreadthFirstPaths bfp = new BreadthFirstPaths(graph, 2);
-        System.out.println(bfp.hasPathTo(0));
-        System.out.println(bfp.pathTo(0));
+        BreadthFirstPaths bfp = new BreadthFirstPaths(graph, 0);
+        System.out.println(bfp.hasPathTo(6));
+        System.out.println(bfp.pathTo(6));
+        System.out.println(bfp.pathTo(6).size());
 
     }
 }
